ec3b3d029b30c81ab362ff3107bfc7bddef14d26
pawluczuk <pawluczuk.monika@gmail.com>
2014-11-30 19:30:00 +0100

    created app skeleton

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'GoldenLine extended' });
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Kontakt' });
+});
+
+router.get('/info', function(req, res) {
+  res.render('info', { title: 'O projekcie' });
+});
+
+router.get('/categories', function(req, res) {
+  res.render('categories', { title: 'Kategorie' });
 });
 
 module.exports = router;
